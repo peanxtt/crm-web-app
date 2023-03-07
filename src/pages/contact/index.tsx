f9d79ccb91c3d68ddcebc214f29c838018ec0e42
peanxtt <94678908+peanxtt@users.noreply.github.com>
2023-03-07 23:21:41 +0800

    adjust pages component

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,33 +1,51 @@
 import Head from "next/head";
 import type { NextPage } from "next";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
 import { Box, TextField, Container, Typography } from "@mui/material";
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
 import { dehydrate, QueryClient, useQuery } from '@tanstack/react-query';
 
-import type { characterType } from "../../../models";
-import Logo from "../../../components/Icons/Logo";
+import type { charactersPreviewType } from "../../../models";
 
 import api from '../api'
+import Logo from "../../../components/Icons/Logo";
+import StickyTable from "../../../components/StickyTable";
 import Loading from "../../../components/Loading";
 
 
 const Contact: NextPage = () => {
+  const router = useRouter();
   const [page, setPage] = useState(1);
-   const charactersQuery = useQuery< characterType[] | null >({
+  const {data: characters, error: charactersError, isLoading: characterIsLoading} = useQuery< charactersPreviewType[] | null >({
     queryKey: ['characters'],
     queryFn: async () => {
       const characters = await api.getAllCharacter(page);
       if(!characters) return null;
       return characters;
-    }
+    },
+    refetchInterval: 10000,
+    staleTime: Infinity,
   });
-  const loading = charactersQuery.isLoading;
 
-  console.log(charactersQuery.data);
+  const columns: GridColDef[] = [
+    // { field: 'id', headerName: 'ID', width: 70},
+    { field: 'name', headerName: 'Name', width: 300 },
+    { field: 'status', headerName: 'Status', width: 150 },
+    { field: 'species', headerName: 'Species', width: 150 },
+    { field: 'gender', headerName: 'Gender', width: 150 },
+  ];
 
-  // if(loading) return <Loading />
+  console.log(characters);
+
+  useEffect(() => {
+    if(charactersError){
+      console.log('character error');
+    }
+  }, [charactersError])
+
+  if(!characters) return <Loading />
   return (
     <>
       <Container maxWidth="md" sx={{ px: 0 }}>
@@ -46,12 +64,13 @@ const Contact: NextPage = () => {
             <Logo />
         </Box>
         <Box sx={{ textAlign: "left", mt: "46px", mb: "30px" }}>
-          <Typography>Contacts</Typography>
-          <TextField
-          label="Search field"
-          type="search"
-          variant="filled"
-        />
+          {/* <Typography>Contacts</Typography> */}
+          <Box sx={{ mb: 3 }}>
+          <TextField id="search-box" label="Search Contact" type="search" variant="outlined" color="success" />
+          </Box>
+          <Box style={{ height: 400, width: '100%' }}>
+            <StickyTable datas={characters} router={router} />
+          </Box>
         </Box>
       </Container>
     </>
---
+++ src/pages/individualContact/index.tsx
@@ -0,0 +1,114 @@
+import Head from "next/head";
+import type { NextPage } from "next";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
+import { Box, Button, Container, Divider, Typography} from "@mui/material";
+import { dehydrate, QueryClient, useQuery } from '@tanstack/react-query';
+import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
+
+import type { characterType } from "../../../models";
+
+import api from '../api'
+import Logo from "../../../components/Icons/Logo";
+import DataTable from "../../../components/DataTable";
+import Information from "../../../components/Information";
+import Loading from "../../../components/Loading";
+
+
+const IndividualContact: NextPage = () => {
+  const router = useRouter();
+  const [charId, setCharId] = useState<string>("");
+
+  useEffect(() => {
+    if (router.isReady) {
+      const { id } = router.query;
+      setCharId(`${id}`);
+    }
+  }, [router.isReady])
+
+  const {data: character, error: characterError} = useQuery< characterType | null >({
+    queryKey: ['character', charId],
+    queryFn: async () => {
+      if(!charId) return null;
+      const character = await api.getCharacter(charId);
+      if(!character) return null;
+      return character;
+    },
+    refetchInterval: 10000,
+    staleTime: Infinity,
+  });
+
+  console.log(character);
+
+  useEffect(() => {
+    if(characterError){
+      console.log('character error');
+    }
+  }, [characterError])
+
+  const handleBack = () => {
+    router.push('/contact');
+  }
+
+  if(!character) return <Loading />
+  return (
+    <>
+      <Container maxWidth="md" sx={{ px: 0 }}>
+        <Head>
+          <title>Character Detail</title>
+          <meta name="" content="Individual Character" />
+        </Head>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            textAlign: "center",
+            mb: 6,
+          }}
+        >
+            <Logo />
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            textAlign: "center",
+            mb: 6,
+          }}
+        >
+          <Box
+            component="img"
+            sx={{
+              border: 1,
+              borderColor: '#00994C',
+              borderRadius: '50%',
+              maxHeight: { xs: 300, md: 150 },
+              maxWidth: { xs: 300, md: 150 },
+            }}
+            alt="Character Selfie"
+            src={character.image}
+          />
+        </Box>
+        <Divider>
+            <Typography sx={{ fontSize: "20px", color: "#000000" }}>Character Details</Typography>
+        </Divider>
+        <Box sx={{ textAlign: "left", mt: "30px", mb: "30px" }}>
+          <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+            <Information information={character} />
+
+            <DataTable />
+          </Box>
+          <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', pt: '175px' }}>
+            <Button onClick={handleBack} aria-label="back" sx={{ color: '#808080', marginBottom: '40px' }}>
+              <KeyboardBackspaceIcon sx={{ color: 'black' }} />
+              <Typography sx={{ color: 'black', fontSize: '20px', ml: '8px' }}>Go Back</Typography>
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </>
+  )
+}
+
+export default IndividualContact;
