24fa515c3f172c4094ccb854527eab1873faaac6
peanxtt <94678908+peanxtt@users.noreply.github.com>
2023-03-07 23:21:17 +0800

    update api requirement and functions

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -1,18 +1,36 @@
-import { charactersSchema, locationSchema, episodeSchema } from "../../../models";
+import {
+  characterSchema,
+  charactersSchema,
+  charactersPreviewSchema,
+  locationSchema,
+  episodeSchema
+} from "../../../models";
 
 const API_URL = process.env.API_URL;
 
 export async function getAllCharacter(page: number) {
-  const res = await fetch(`${API_URL}character/?page=${page}`, {
+  const res = await fetch(`${API_URL}/character/?page=${page}`, {
     method: "GET",
   })
   if(!res.ok) return undefined;
   const json = await res.json();
-  if(!charactersSchema.safeParse(json.results).success) throw 'invalid_character_schema';
-  const data = charactersSchema.parse(json.results);
+  if(!charactersSchema.safeParse(json.results).success) throw 'invalid_characters_schema';
+  const data = charactersPreviewSchema.parse(json.results);
+  return data;
+}
+
+export async function getCharacter(id: string) {
+  const res = await fetch(`${API_URL}/character/${id}`, {
+    method: "GET",
+  })
+  if(!res.ok) return undefined;
+  const json = await res.json();
+  if(!characterSchema.safeParse(json).success) throw 'invalid_character_schema';
+  const data = characterSchema.parse(json);
   return data;
 }
 
 export default {
-  getAllCharacter
+  getAllCharacter,
+  getCharacter
 }
